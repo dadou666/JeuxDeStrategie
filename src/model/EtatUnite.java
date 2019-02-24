f0c28fc968f044e2bb7c6da88518d9a1dd5f3f38
dadou666 <david.besnard666@gmail.com>
2019-02-24 10:59:22 +0100

    ok

--- src/model/EtatUnite.java
+++ src/model/EtatUnite.java
@@ -5,5 +5,6 @@ public abstract class EtatUnite {
 	public abstract void executer(Unite unite,Plateau plateau) ;
 	abstract public void finDeplacer(Unite unite,Plateau plateau) ;
 	public abstract void liberer(Unite unite,Plateau p);
+	
 
 }
